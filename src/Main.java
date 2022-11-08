38d855c3f1b1f7d18054c38590614c743c489ccd
vishal8202 <vishalvijayan222@gmail.com>
2022-11-08 22:59:01 +0530

    view

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
@@ -38,7 +36,7 @@ public class Main {
                         Class.forName("com.mysql.jdbc.Driver");
                         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb","root","");
                         String sql = "INSERT INTO `students`(`name`, `rollnumber`, `admno`, `college`) VALUES (?,?,?,?)";
-                        PreparedStatement stmt = con.prepareStatement(sql);
+                        PreparedStatement stmt = con.prepareStatement(sql); //to pass the data to the database
                         stmt.setString(1,name);
                         stmt.setInt(2,rollno);
                         stmt.setInt(3,admnum);
@@ -54,6 +52,29 @@ public class Main {
                     break;
                 case 2:
                     System.out.println("View all student");
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb","root","");
+                        String sql = "SELECT `name`, `rollnumber`, `admno`, `college` FROM `students`";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(sql);
+                        while (rs.next()){
+
+                            String getName = rs.getString("name");
+                            String getRollno = rs.getString("rollnumber");
+                            String getAdmno = rs.getString("admno");
+                            String getCollege = rs.getString("college");
+                            System.out.println("Name="+getName);
+                            System.out.println("Rollno="+getRollno);
+                            System.out.println("Admno="+getAdmno);
+                            System.out.println("college="+getCollege+"\n");
+                        }
+
+
+                    }
+                    catch (Exception e ){
+                        System.out.println(e);
+                    }
                     break;
                 case 3:
                     System.out.println("Search student");
